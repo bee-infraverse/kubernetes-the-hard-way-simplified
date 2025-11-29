d3a256cdbbc8d4073aa6a617f73ad08204a45fb8
Peter Rossbach <peter.rossbach@bee42.com>
2025-11-29 09:51:25 +0100

    Update: Deps
    Signed-off-by: Peter Rossbach <peter.rossbach@bee42.com>

--- bootstrap/env.sh
+++ bootstrap/env.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 export ARCH=$(dpkg --print-architecture)
-export K8S_VERSION=${K8S_VERSION:-v1.34.0} # renovate: datasource=github-tags depName=kubernetes/kubernetes
+export K8S_VERSION=${K8S_VERSION:-v1.34.2} # renovate: datasource=github-tags depName=kubernetes/kubernetes
 export CRICTL_VERSION=${CRICTL_VERSION:-v1.34.0} # renovate: datasource=github-releases depName=kubernetes-sigs/cri-tools
-export RUNC_VERSION=${RUNC_VERSION:-v1.3.1} # renovate: datasource=github-releases depName=opencontainers/runc
+export RUNC_VERSION=${RUNC_VERSION:-v1.3.4} # renovate: datasource=github-releases depName=opencontainers/runc
 export CNI_PLUGINS=${CNI_PLUGINS:-v1.7.1} # renovate: datasource=github-releases depName=containernetworking/plugins
-export CONTAINERD_VERSION=${CONTAINERD_VERSION:-2.1.4} # renovate: datasource=github-releases depName=containerd/containerd
+export CONTAINERD_VERSION=${CONTAINERD_VERSION:-2.1.5} # renovate: datasource=github-releases depName=containerd/containerd
 export ETCD_VERSION=${ETCD_VERSION:-v3.6.5} # renovate: datasource=github-releases depName=etcd-io/etcd
 export NERDCTL_VERSION=${NERDCTL_VERSION:-2.1.6} # renovate: datasource=github-releases depName=containerd/nerdctl
-export HELM_VERSION=${HELM_VERSION:-v3.19.0} # renovate: datasource=github-releases depName=helm/helm
+export HELM_VERSION=${HELM_VERSION:-v3.19.2} # renovate: datasource=github-releases depName=helm/helm
 export CLUSTER_DOMAIN=${CLUSTER_DOMAIN:-cluster.local}
 export COUNTRY="${COUNTRY:-DE}"
 export CITY="${CITY:-Bochum}"
